eef5be31bd7e3bf711f6613cf3ea17867583ee6e
Rohan Mishra <rohanmishra.004@gmail.com>
2023-06-22 15:54:12 +0530

    Check added for user

--- server.js
+++ server.js
@@ -22,6 +22,26 @@ app.post('/users', async (req, res) => {
     res.send('User added')
 })
 
+
+
+//call to check password
+app.post('/users/login', async(req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user === null) {
+        return res.status(400).send('No user found')
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch (err) {
+        return res.status(500).send()
+    }
+})
+
+
 app.listen(3000, () => {
     console.log('Server running on port 3000')
 })
